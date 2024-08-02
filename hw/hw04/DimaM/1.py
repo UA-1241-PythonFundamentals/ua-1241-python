493b81b0aa488a98fddf5e881a20bbf558eb544b
DimaMakovskyi <dmutromakovskiy@gmail.com>
2024-08-02 20:11:11 +0300

    dimamakovskiyHW4

---
+++ hw/hw04/DimaM/1.py
@@ -0,0 +1,6 @@
+temperature = float(input("Enter the temperature in Celsius: "))
+if temperature < -271.15:
+    print("Error:Temperature below absolute zero(-273.15°С)")
+else:
+    in_F = temperature * 9/5 + 32
+    print(f"{temperature}°C is equivalent to {in_F}°F")
