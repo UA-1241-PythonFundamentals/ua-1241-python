0c44ff404e6c1f27f1e90abd9ca3ef1ceb0e1f11
Yurii Kopka <gerraldofrivia@gmail.com>
2024-08-05 22:27:12 +0200

    add fix

--- hw/hw04/KopkaYurii/Hw4.py
+++ hw/hw04/KopkaYurii/Hw4.py
@@ -3,4 +3,4 @@ if TempCel < -273.15:
         print("Error: Temperature below absolute zero (-273.15°C)")
 else:
     tempFar = (TempCel * 9/5) + 32
-    print(f"{TempCel}C is equivalent to {tempFar}F")
+    print(f"{TempCel}°C is equivalent to {tempFar}F")
