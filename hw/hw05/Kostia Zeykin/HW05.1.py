b1c731d6b3fabb139859c8c1f7246deaf2931d8c
koskin <kostia.zeykin@gmail.com>
2024-08-10 18:09:14 +0300

    Task 1 of HW 05.1 by Kostia Zeykin

---
+++ hw/hw05/Kostia Zeykin/HW05.1.py
@@ -0,0 +1,14 @@
+"""Task 1"""
+
+start_list = []
+for element in range(20):
+    start_list.append(element)
+
+end_list = []
+for element in start_list:
+    end_list.append(float(element))
+
+print(end_list)
+
+"""Task 2"""
+
