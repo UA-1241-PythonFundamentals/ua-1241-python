a14df9ae619603f3ac4b10e46c0a48892e10b502
koskin <118272123+koskin17@users.noreply.github.com>
2024-09-07 14:58:24 +0300

    Task 3 of HW10 is done

--- hw/hw10/Kostia Zeykin/HW10.1.py
+++ hw/hw10/Kostia Zeykin/HW10.1.py
@@ -1,49 +1,46 @@
-# """Task 1"""
-# class Polygon:
-#     pass
-#
-# class Rectangle(Polygon):
-#
-#     def __init__(self):
-#         super().__init__()
-#
-#     def square(self, x, y):
-#         return x * y
-#
-# rectangle = Rectangle()
-# side1 = float(input("Укажите первую сторону прямоугольника: "))
-# side2 = float(input("Укажите вторую сторону прямоугольника: "))
-# print(f"Площадь прямоугольника со сторонами {side1} и {side2} равна: {rectangle.square(side1, side2)}")
-#
-# """Task 2"""
-# class Human:
-#     spec = "Homosapiens"
-#
-#     def __init__(self, name):
-#         self.name = name
-#
-#     def greeting(self):
-#         print(f"Добро пожаловать, {self.name}")
-#
-#     @classmethod
-#     def species(cls):
-#         print(f"Этот класс создан для {cls.spec}")
-#
-#     @staticmethod
-#     def message():
-#         print("Это произвольное сообщение.")
-#
-#
-# user_name = input("Представьтесь, пожалуйста: ")
-# user_name = Human(user_name)
-# user_name.greeting()
-# user_name.species()
-# user_name.message()
+"""Task 1"""
+class Polygon:
+    pass
+
+class Rectangle(Polygon):
+
+    def __init__(self):
+        super().__init__()
+
+    def square(self, x, y):
+        return x * y
+
+rectangle = Rectangle()
+side1 = float(input("Укажите первую сторону прямоугольника: "))
+side2 = float(input("Укажите вторую сторону прямоугольника: "))
+print(f"Площадь прямоугольника со сторонами {side1} и {side2} равна: {rectangle.square(side1, side2)}")
+
+"""Task 2"""
+class Human:
+    spec = "Homosapiens"
+
+    def __init__(self, name):
+        self.name = name
+
+    def greeting(self):
+        print(f"Добро пожаловать, {self.name}")
+
+    @classmethod
+    def species(cls):
+        print(f"Этот класс создан для {cls.spec}")
+
+    @staticmethod
+    def message():
+        print("Это произвольное сообщение.")
+
+
+user_name = input("Представьтесь, пожалуйста: ")
+user_name = Human(user_name)
+user_name.greeting()
+user_name.species()
+user_name.message()
 
 """Task 3"""
-from statistics import quantiles
-
-
 class Employee:
     """Class about employees: surname and salary"""
     quantity = 0
